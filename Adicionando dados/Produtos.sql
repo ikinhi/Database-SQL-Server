77c8d2d63a982c176ea44a07546e41282743fff3
ikinhi <blyaternita@hotmail.com>
2022-09-07 11:18:30 -0300

    Produtos adicionados

---
+++ Adicionando dados/Produtos.sql
@@ -0,0 +1,10 @@
+use BASEDEDADOS
+
+insert into PRODUTOS values
+('Computador',3000.00,'Tecnologia'),
+('Livro de SQL',20.00,'Livro'),
+('Garrafa de água',4.00,'Alimento'),
+('Estante',350.00,'Mobília'),
+('Monito',1200.00,'Tecnologia')
+
+go
